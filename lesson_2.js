bcfc5eeafad90a1665370cefa57ad6351d0ada85
Abel Woldemichael <abelllselam@gmail.com>
2025-04-13 18:25:58 -0400

    The end of lesson 2 where we covered forEach, and different types of functons.

--- lesson_2.js
+++ lesson_2.js
@@ -24,3 +24,28 @@
 
 // var result = add(2, 3); //5
 // console.log(result); //undefined as there is nothing being returned
+
+// //anonymous functions
+// var anonFunc = function () {
+//   console.log("This is an anonymous function");
+// };
+
+// //regular function
+// function regFunc(param1) {
+//   console.log(
+//     "This is a regular function and i am also going to pring the param1 here: " +
+//       param1
+//   );
+// }
+// anonFunc();
+// regFunc(2);
+
+//for loop forEach
+
+var arrs = [1, 2, 3, 4, 5, 6];
+
+arrs.forEach((arr, index, array) => {
+  console.log(
+    `This is the current index: ${index} and the array value is ${arr} and the array is ${array}`
+  );
+});
