6e74fca7e5e5d41b6928f29c85ef5ac94787dab6
Abel Woldemichael <abelllselam@gmail.com>
2025-04-13 17:01:32 -0400

    started lesson_2 functions.

---
+++ lesson_2.js
@@ -0,0 +1,26 @@
+// //if else and equality of == and === test and lesson
+// var x = 4;
+// if (x === "4") {
+//   console.log("It is true");
+// } else {
+//   console.log("It is false");
+// }
+
+// //functions nonvoid
+// function add(x, y) {
+//   return x + y;
+// }
+
+// result = add(2, 3);
+
+// console.log(result);
+
+// //function void
+// function add(x, y) {
+//   var sum = x + y;
+
+//   console.log(sum);
+// }
+
+// var result = add(2, 3); //5
+// console.log(result); //undefined as there is nothing being returned
